349edc66f770a6e6f43e793651e5c0a42a997ef7
sntty <8don.pokopoko@gmail.com>
2021-07-08 11:43:39 +0900

    henkou

--- OIC_PBL/OIC_PBL/2-1コロナ対応画面1.Designer.cs
+++ OIC_PBL/OIC_PBL/2-1コロナ対応画面1.Designer.cs
@@ -37,9 +37,11 @@
             // button1
             // 
             this.button1.BackColor = System.Drawing.Color.DeepSkyBlue;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.ForeColor = System.Drawing.SystemColors.ControlLightLight;
             this.button1.Location = new System.Drawing.Point(72, 196);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(263, 152);
+            this.button1.Size = new System.Drawing.Size(263, 153);
             this.button1.TabIndex = 0;
             this.button1.Text = "岡山県外";
             this.button1.UseVisualStyleBackColor = false;
@@ -48,6 +50,8 @@
             // button2
             // 
             this.button2.BackColor = System.Drawing.Color.DeepSkyBlue;
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button2.ForeColor = System.Drawing.SystemColors.ControlLightLight;
             this.button2.Location = new System.Drawing.Point(470, 196);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(263, 153);
@@ -83,6 +87,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 18F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Window;
             this.ClientSize = new System.Drawing.Size(800, 450);
             this.Controls.Add(this.linkLabel1);
             this.Controls.Add(this.label1);
