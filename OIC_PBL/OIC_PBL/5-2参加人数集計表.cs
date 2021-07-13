c10755d152e68ea34e2565d1ca88efc9bc74cd38
askhidechan <86337759+askhidechan@users.noreply.github.com>
2021-07-13 15:15:39 +0900

    5-2

--- OIC_PBL/OIC_PBL/5-2参加人数集計表.cs
+++ OIC_PBL/OIC_PBL/5-2参加人数集計表.cs
@@ -24,5 +24,10 @@ namespace OIC_PBL
 
             this.Hide();
         }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
     }
 }
