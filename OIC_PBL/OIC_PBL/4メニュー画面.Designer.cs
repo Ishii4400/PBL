f6ff8d61cbb17f7ff04523b7e73f88f06a9e8479
askhidechan <86337759+askhidechan@users.noreply.github.com>
2021-07-14 12:39:05 +0900

    no message

--- OIC_PBL/OIC_PBL/4メニュー画面.Designer.cs
+++ OIC_PBL/OIC_PBL/4メニュー画面.Designer.cs
@@ -34,7 +34,8 @@
             this.TOP = new System.Windows.Forms.LinkLabel();
             this.label2 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
-            this.button1 = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel1.SuspendLayout();
             this.SuspendLayout();
             // 
             // label1
@@ -43,7 +44,7 @@
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
             this.label1.BackColor = System.Drawing.SystemColors.MenuBar;
-            this.label1.Location = new System.Drawing.Point(70, 89);
+            this.label1.Location = new System.Drawing.Point(54, 60);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(491, 203);
             this.label1.TabIndex = 4;
@@ -59,7 +60,7 @@
             this.一般教員用.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.一般教員用.Font = new System.Drawing.Font("MS UI Gothic", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
             this.一般教員用.ForeColor = System.Drawing.SystemColors.ControlLightLight;
-            this.一般教員用.Location = new System.Drawing.Point(90, 120);
+            this.一般教員用.Location = new System.Drawing.Point(74, 91);
             this.一般教員用.Margin = new System.Windows.Forms.Padding(2);
             this.一般教員用.Name = "一般教員用";
             this.一般教員用.Size = new System.Drawing.Size(205, 142);
@@ -78,7 +79,7 @@
             this.上長用.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.上長用.Font = new System.Drawing.Font("MS UI Gothic", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
             this.上長用.ForeColor = System.Drawing.SystemColors.ControlLightLight;
-            this.上長用.Location = new System.Drawing.Point(319, 120);
+            this.上長用.Location = new System.Drawing.Point(303, 91);
             this.上長用.Margin = new System.Windows.Forms.Padding(2);
             this.上長用.Name = "上長用";
             this.上長用.Size = new System.Drawing.Size(220, 142);
@@ -94,7 +95,7 @@
             | System.Windows.Forms.AnchorStyles.Right)));
             this.TOP.AutoSize = true;
             this.TOP.LinkColor = System.Drawing.Color.DeepSkyBlue;
-            this.TOP.Location = new System.Drawing.Point(60, 320);
+            this.TOP.Location = new System.Drawing.Point(44, 291);
             this.TOP.Name = "TOP";
             this.TOP.Size = new System.Drawing.Size(36, 15);
             this.TOP.TabIndex = 3;
@@ -111,7 +112,7 @@
             this.label2.BackColor = System.Drawing.Color.DeepSkyBlue;
             this.label2.Font = new System.Drawing.Font("MS UI Gothic", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
             this.label2.ForeColor = System.Drawing.SystemColors.HighlightText;
-            this.label2.Location = new System.Drawing.Point(108, 225);
+            this.label2.Location = new System.Drawing.Point(92, 196);
             this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(168, 14);
@@ -127,21 +128,25 @@
             this.label3.BackColor = System.Drawing.Color.DeepSkyBlue;
             this.label3.Font = new System.Drawing.Font("MS UI Gothic", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
             this.label3.ForeColor = System.Drawing.SystemColors.HighlightText;
-            this.label3.Location = new System.Drawing.Point(399, 225);
+            this.label3.Location = new System.Drawing.Point(383, 196);
             this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(63, 14);
             this.label3.TabIndex = 6;
             this.label3.Text = "合否承認";
             // 
-            // button1
+            // panel1
             // 
-            this.button1.Location = new System.Drawing.Point(134, 29);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 7;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.TOP);
+            this.panel1.Controls.Add(this.上長用);
+            this.panel1.Controls.Add(this.一般教員用);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Location = new System.Drawing.Point(16, 29);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(570, 330);
+            this.panel1.TabIndex = 7;
             // 
             // メニュー画面
             // 
@@ -149,19 +154,14 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.Window;
             this.ClientSize = new System.Drawing.Size(640, 375);
-            this.Controls.Add(this.button1);
-            this.Controls.Add(this.label3);
-            this.Controls.Add(this.label2);
-            this.Controls.Add(this.TOP);
-            this.Controls.Add(this.上長用);
-            this.Controls.Add(this.一般教員用);
-            this.Controls.Add(this.label1);
+            this.Controls.Add(this.panel1);
             this.Margin = new System.Windows.Forms.Padding(2);
             this.Name = "メニュー画面";
             this.Text = "メニュー画面";
             this.Load += new System.EventHandler(this.メニュー画面_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
@@ -173,6 +173,6 @@
         private System.Windows.Forms.LinkLabel TOP;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Panel panel1;
     }
 }
