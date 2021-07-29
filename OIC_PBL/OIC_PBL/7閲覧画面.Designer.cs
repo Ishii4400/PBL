990a4eae1c166be9e1fef42fd0e959e19d34191b
askhidechan <86337759+askhidechan@users.noreply.github.com>
2021-07-29 10:15:17 +0900

    7

--- OIC_PBL/OIC_PBL/7閲覧画面.Designer.cs
+++ OIC_PBL/OIC_PBL/7閲覧画面.Designer.cs
@@ -170,7 +170,7 @@
             this.dataGridView1.Name = "dataGridView1";
             this.dataGridView1.RowHeadersWidth = 51;
             this.dataGridView1.RowTemplate.Height = 24;
-            this.dataGridView1.Size = new System.Drawing.Size(1674, 707);
+            this.dataGridView1.Size = new System.Drawing.Size(1674, 748);
             this.dataGridView1.TabIndex = 7;
             // 
             // 参加者number
